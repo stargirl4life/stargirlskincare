1f693f8eaaaeaf5a88c52a4e49b48718156ea2d1
ikenna <mrteks2022@gmail.com>
2023-06-24 02:36:49 -0700

    first commit

---
+++ utils/data.js
@@ -0,0 +1,111 @@
+
+import bcrypt from 'bcryptjs';
+
+
+//creating data object users and product arrays
+// so here we have created two users and six product inside the users and product array
+//in data object
+const data = {
+  users: [
+    {
+      name: 'Erica',
+      email: 'stargirl@gmail.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: true,
+    },
+    {
+      name: 'Jane',
+      email: 'user@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: false,
+    },
+  ],
+
+  products: [
+    {
+      name: 'illuminating glow scrub',
+      slug: 'free-glow face scrub',
+      category: 'skincare',
+      image: '/images/ericaimage3.jpg',
+      price: 70,
+      brand: 'Stargirl skincare',
+      rating: 4.5,
+      numReviews: 1000,
+      countInStock: 20,
+      description: 'smoothen and soften the body',
+      isFeatured: true,
+      banner: '/images/ericaimage4.jpg',
+    },
+    {
+      name: 'Body lotion',
+      slug: 'free-smoothen skin cream',
+      category: 'Skincare',
+      image: '/images/ericaimage3(2).jpg',
+      price: 80,
+      brand: 'Stargirl skincare',
+      rating: 3.2,
+      numReviews: 1000,
+      countInStock: 20,
+      description: 'specifically tailored to skin types and tones',
+      isFeatured: true,
+      banner: '/images/ericaimage3.jpg',
+    },
+    {
+      name: 'body lotion',
+      slug: 'free-cream',
+      category: 'skincare',
+      image: '/images/ericaimage4.jpg',
+      price: 90,
+      brand: 'Stargirl skincare',
+      rating: 4.5,
+      numReviews: 3,
+      countInStock: 20,
+      description: 'specifically tailored to skin types and tones',
+    },
+    {
+      name: 'body scrub',
+      slug: 'free-facial scrub',
+      category: 'skincare',
+      image: '/images/ericaimage2.jpg',
+      price: 90,
+      brand: 'Stargirl skincare',
+      rating: 2.9,
+      numReviews: 13,
+      countInStock: 20,
+      description: 'Smart looking designers shoes',
+    },
+    {
+      name: 'Stargirl skincare',
+      slug: 'free-care',
+      category: 'skincare',
+      image: '/images/ericaimage3(2).jpg',
+      price: 95,
+      brand: 'Stargirl skincare',
+      rating: 3.5,
+      numReviews: 7,
+      countInStock: 20,
+      description: 'body,Anti-aging Detox',
+    },
+    {
+      name: 'Stargirl skincare',
+      slug: 'free-body care',
+      category: 'skincare',
+      image: '/images/.jpg',
+      price: 75,
+      brand: 'Stargirl skincare',
+      rating: 2.4,
+      numReviews: 14,
+      countInStock: 20,
+      description: '',
+    },
+  ],
+};
+
+
+
+
+
+
+export default data;
+
+// we export the default
